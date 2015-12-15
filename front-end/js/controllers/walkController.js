95e691c957c71976e813ea90a0a1642e67cbe36a
siyounguk <si.younguk@gmail.com>
2015-12-15 10:17:22 +0000

    can display routes

--- front-end/js/controllers/walkController.js
+++ front-end/js/controllers/walkController.js
@@ -19,6 +19,8 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
   };
   route = self.route
 
+  var checkboxArray = []
+
   uiGmapGoogleMapApi.then(function(maps) {
     var polylineOptions = new maps.Polyline({
       strokeColor: 'blue',
@@ -28,10 +30,12 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
     self.map = new maps.Map(document.getElementById('main-map'), { center: { lat: 51.5081, lng: -0.1000 }, zoom: 14 });
     // $scope.options = { scrollwheel: false };
 
+
     self.directionsService = new maps.DirectionsService;
     self.directionsDisplay = new maps.DirectionsRenderer(rendererOptions)
     self.directionsDisplay.suppressMarkers = true
 
+    self.directionsDisplay.setMap(self.map);
     var rendererOptions = {
       map: self.map,
       suppressInfoWindows: true,
@@ -39,13 +43,11 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
       polylineOptions: polylineOptions
     };
 
-    // var infowindow = new maps.InfoWindow();
-    // var marker = new maps.Marker({
-    //   map: self.map,
-    //   anchorPoint: new maps.Point(0, -29)
-    // });
-    // self.description = document.getElementById('description').value;
-    // self.photo = document.getElementById('photo').value;
+    self.infowindow = new maps.InfoWindow();
+    self.marker = new maps.Marker({
+      map: self.map,
+      anchorPoint: new maps.Point(0, -29)
+    });
 
     self.input = document.getElementById('pac-input');
     self.autocomplete = new maps.places.Autocomplete(self.input);
@@ -54,32 +56,33 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
     self.startInput = document.getElementById('start');
     var startAutocomplete = new maps.places.Autocomplete(self.startInput);
     startAutocomplete.bindTo('bounds', self.map);
-    var originPlace = startAutocomplete.getPlace()
+    self.originPlace = startAutocomplete.getPlace()
    
     self.endInput = document.getElementById('end');
     var endAutocomplete = new maps.places.Autocomplete(self.endInput);
     endAutocomplete.bindTo('bounds', self.map);
-    var destinationPlace = endAutocomplete.getPlace()
+    self.destinationPlace = endAutocomplete.getPlace()
 
     self.addRoute = function(){
-      event.preventDefault()
+      self.originPlace = startAutocomplete.getPlace()
+      console.log(self.originPlace)
       self.route.origin = {
-        place_id: originPlace.id,
-        lat: originPlace.geometry.location.lat(),
-        lng: originPlace.geometry.location.lng(),
-        formatted_address: originPlace.formatted_address,
-        name: originPlace.name
+        place_id: self.originPlace.place_id,
+        lat: self.originPlace.geometry.location.lat(),
+        lng: self.originPlace.geometry.location.lng(),
+        formatted_address: self.originPlace.formatted_address,
+        name: self.originPlace.name
       }
       routeOrigin = self.route.origin
       route.origin = routeOrigin
   
       
       self.route.destination = {
-        place_id: destinationPlace.id,
-        lat: destinationPlace.geometry.location.lat(),
-        lng: destinationPlace.geometry.location.lng(),
-        formatted_address: destinationPlace.formatted_address,
-        name: destinationPlace.name
+        place_id: self.destinationPlace.place_id,
+        lat: self.destinationPlace.geometry.location.lat(),
+        lng: self.destinationPlace.geometry.location.lng(),
+        formatted_address: self.destinationPlace.formatted_address,
+        name: self.destinationPlace.name
       }
       routeDestination = self.route.destination
       route.destination = routeDestination
@@ -101,12 +104,16 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
     }
 
     self.calculateAndDisplayRoute = function (directionsService, directionsDisplay) {
-      console.log("Clicked")
-      console.log(originPlace)
+      
       // console.log(originPlaceId)
       // if (originPlace.photos){
       //   console.log(originPlace.photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35}))
       // }
+      var originPlace = startAutocomplete.getPlace()
+      var destinationPlace = endAutocomplete.getPlace()
+      console.log(originPlace)
+      console.log(originPlace.geometry.location)
+
           
 
       var waypts = [];
@@ -119,29 +126,29 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
         // placesList.innerHTML += '<li>' + checkboxArray[i].name + '<img src='+checkboxArray.photos[0].getUrl({'maxWidth': 100, 'maxHeight': 100})+'>'+'</li>'
       }
 
-      directionsService.route({
-        origin: originPlace.formatted_address,
-        destination: destinationPlace.formatted_address,
+      self.directionsService.route({
+        origin: originPlace.geometry.location,
+        destination: destinationPlace.geometry.location,
         waypoints: waypts,
         optimizeWaypoints: true,
         travelMode: google.maps.TravelMode.WALKING,
       }, function(response, status) {
-        directionsDisplay.suppressMarkers = true
+        self.directionsDisplay.suppressMarkers = true
         if (status === google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
+          self.directionsDisplay.setDirections(response);
           var route = response.routes[0];
 
-          // var summaryPanel = document.getElementById('directions-panel');
-          // summaryPanel.innerHTML = '';
-          // // For each route, display summary information.
-          // for (var i = 0; i < route.legs.length; i++) {
-          //   var routeSegment = i + 1;
-          //   summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-          //       '</b><br>';
-          //   summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-          //   summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-          //   summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-          // }
+          var summaryPanel = document.getElementById('directions-panel');
+          summaryPanel.innerHTML = '';
+          // For each route, display summary information.
+          for (var i = 0; i < route.legs.length; i++) {
+            var routeSegment = i + 1;
+            summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                '</b><br>';
+            summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+            summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+            summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+          }
         } else {
           window.alert('Directions request failed due to ' + status);
         }
@@ -149,6 +156,44 @@ function WalksController($window, $scope ,$resource, Walk, uiGmapGoogleMapApi, T
     }
 
   });
+
+  self.addLocation = function (){
+    self.infowindow.close();
+    self.marker.setVisible(false);
+    var place = self.autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+    checkboxArray.push(place)
+    self.calculateAndDisplayRoute(self.directionsService, self.directionsDisplay)
+    if (place.geometry.viewport) {
+      self.map.fitBounds(place.geometry.viewport);
+    } else {
+      self.map.setCenter(place.geometry.location);
+      self.map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    self.marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    self.marker.setPosition(place.geometry.location);
+    self.marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+
+  }
   
     
 
